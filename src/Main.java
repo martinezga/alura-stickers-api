05d5831a69b42a1712f43316253d5a1f08c95134
martinezga <52822208+martinezga@users.noreply.github.com>
2022-07-18 17:38:41 -0300

    Colored terminal output

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,10 @@ public class Main {
         String API_KEY = System.getenv("API_KEY");
         String URL = "https://imdb-api.com/en/API/Top250Movies/" + API_KEY;
         String MOCK = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        String bold = "\u001b[1m";
+        String notBold = "\u001b[0m";
+        String colored = "\u001b[30m\u001b[45m";
+        String rating;
 
         // Get top 250 movies
         URI myURI = URI.create(MOCK);
@@ -25,9 +29,12 @@ public class Main {
 
         // Show info: title, image, rank
         for (Map<String, String> film : dataList) {
-            System.out.println(film.get("fullTitle"));
-            System.out.println(film.get("image"));
-            System.out.println(film.get("imDbRating"));
+            rating = film.get("imDbRating");
+
+            System.out.printf("%sTitle (year): %s%s%n", notBold, bold, film.get("fullTitle"));
+            System.out.printf("%sPoster: %s%s%n", notBold, bold, film.get("image"));
+            System.out.printf("%sRating: %s%n", colored, rating);
+            System.out.printf("%s%s%n", notBold, "⭐️".repeat((int) Double.parseDouble((rating))));
         }
     }
 }
